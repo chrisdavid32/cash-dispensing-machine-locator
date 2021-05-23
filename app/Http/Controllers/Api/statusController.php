8f918541d9f7bc07efd721d4a18e4839e4701773
chrisdavid32 <chrisdavid3271@gmail.com>
2021-05-23 18:00:47 +0100

    create status code functions to statusController

--- app/Http/Controllers/Api/statusController.php
+++ app/Http/Controllers/Api/statusController.php
@@ -7,5 +7,44 @@ use Illuminate\Http\Request;
 
 class statusController extends Controller
 {
-    //
+    public function success($message)
+    {
+        $response = [
+            "message" => "success",
+            "data" =>  $message
+        ];
+        return response()->json($response, 200);
+    }
+
+    public function created($message)
+    {
+        $response = [
+            "message" => "created",
+            "data" =>  $message
+        ];
+        return response()->json($response, 201);
+    }
+
+    public function badrequest($message)
+    {
+        $response = [
+            "message" => "bad request",
+            "data" => ['error' => $message]
+        ];
+        return response()->json($response, 400);
+    }
+
+    public function notfound($message)
+    {
+        $response = [
+            "message" => "not found",
+            "data" => ['error' => $message]
+        ];
+        return response()->json($response, 404);
+    }
+
+    public function severerror($message)
+    {
+        return response()->json(['error' => $message], 500);
+    }
 }
